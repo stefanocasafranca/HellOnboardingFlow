882a03941e565d03d6a0285aff94ae7164adfee4
stefanocasafranca <scasafrancal01@gmail.com>
2025-02-15 10:15:00 -0600

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Stefano Casafranca on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
